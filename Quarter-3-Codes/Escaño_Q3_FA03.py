4f6cbafd4cfde48787445204e81ea01c4b97edf8
Kaelyn Anne D. Escaño <kaelynanne.escano@smc.pshs.edu.ph>
2026-01-13 22:45:29 +0800

    "aha"

--- Quarter-3-Codes/Escaño_Q3_FA03.py
+++ Quarter-3-Codes/Escaño_Q3_FA03.py
@@ -1,3 +1,5 @@
+names = ["Me", "Lia", "Jake"]
+
 steps = [
     ["Me",   4500, 5200, 4800, 5000, 5300],
     ["Lia",  4000, 4100, 3900, 4200, 4600],
@@ -5,3 +7,29 @@ steps = [
 ]
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+print("----- Daily steps -----")
+for x in steps:
+    print(x[0], ": " , x[1:])
+
+overall_max = steps[0][1]
+overall_min = steps[0][1]
+
+for x in steps:
+    name = x[0]
+    
+    Daily_steps = []
+    for i in range(1, len(x)):
+        Daily_steps.append(x[i])
+
+    # Row total and average
+    total = sum(Daily_steps)
+    avg = total / len(Daily_steps)
+    
+    # Update overall max/min
+    person_max = max(Daily_steps)
+    person_min = min(Daily_steps)
+
+    print(name, "- Total:", total, " Average:", avg, " Max:", person_max, " Min:", person_min)
+
+#I used chat gpt to help me with the slicing of array part since I am not yet familiar with it. i'm sorry Ma'am Maui.
